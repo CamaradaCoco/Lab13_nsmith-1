0c1f0d108858ba6e9075d3b37e41d46ef14a073a
Nicholas Smith <nsmith196@student.cscc.edu>
2025-04-11 03:50:45 -0400

    f

---
+++ ship.py
@@ -0,0 +1,42 @@
+import pygame as pg
+
+class Ship:
+    """A class to manage the ship."""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position.
+        
+        args: 
+            ai_game (AlienInvasion): An instance of the AlienInvasion class.
+        """
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # Load the ship image and get its rect.
+        self.image = pg.image.load('images/myspace.webp')
+        self.rect = self.image.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a floating value for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship's position based on the movement flags."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        
+        # Update rect object from self.x.
+        self.rect.x = self.x
